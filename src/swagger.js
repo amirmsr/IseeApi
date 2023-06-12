c3e81a39bf0c6b8a4c31053bd4396052866cc191
Iyad Auckburally <iyad.auckburally@datasense-group.com>
2023-06-12 14:08:00 +0200

    ajout du bon swagger

--- src/swagger.js
+++ src/swagger.js
@@ -1,29 +1,12 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
+import YAML from "yamljs";
 
 const app = express();
 app.use(express.json())
 
-const options = {
-    swaggerDefinition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'iBay API, Iyad Amir Marcel Yohan, BENG3',
-        version: '1.0.0',
-        description: 'Final version',
-      },
-      servers: [
-        {
-          url: 'http://localhost:3000',
-        },
-      ],
-    },
-    apis: ['src/routers/*.js'],
-};
-  
-const specs = swaggerJsdoc(options);
-  
-app.use('/documentation', swaggerUi.serve, swaggerUi.setup(specs));
+const swaggerFile = YAML.load('src/swagger.yaml')
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export default app
